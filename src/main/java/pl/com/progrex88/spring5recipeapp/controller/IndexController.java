c21b4d72c5d2d328e68c1141f69a46d177fe80a8
progrex <progrex88@gmail.com>
2018-11-14 20:15:51 +0100

    add new files

---
+++ src/main/java/pl/com/progrex88/spring5recipeapp/controller/IndexController.java
@@ -0,0 +1,15 @@
+package pl.com.progrex88.spring5recipeapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"","/","/index"})
+    public String getIndexPage(){
+
+        return "index";
+    }
+
+}
